0c955f980b0c3a45542fa117529d79d2c8e5b2a6
Nick Kanakakorn <skanan@nutanix.com>
2016-09-13 23:26:41 +0700

    fix permission

--- ge4misc.sh
+++ ge4misc.sh
@@ -1,3 +1,3 @@
-/Applications/Mist.app/Contents/Frameworks/node/geth/geth --testnet --fast
+/usr/bin/geth --testnet --fast
 --ipcpath /Users/skanan/Library/Ethereum/geth.ipc --rpccorsdomain "*" --rpc
 --rpcport 8545 --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3"
