32214541462b298478dc887a3ccf8faa0e17acd6
ether1 <nick@localhost.com>
2017-05-19 05:37:45 +0700

    make cli.sh for Linux

--- cli.sh
+++ cli.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-/Applications/Mist.app/Contents/Frameworks/node/geth/geth attach \
-ipc://Users/skanan/Library/Ethereum/geth.ipc $*
+geth attach ipc://$HOME/.ethereum/testnet/geth.ipc console
