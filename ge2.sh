29ef6609abe8f662ad5e2d7eec83c296b50f538c
Nick Kanakakorn <nick@blockfint.com>
2017-05-24 13:27:38 +0700

    aa

--- ge2.sh
+++ ge2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-geth --identity "node2" \
+geth --dev --identity "node2" \
   --datadir "$HOME/myetherdata/testdata2" \
   --rpc --rpcport "8102" --rpccorsdomain "*" --port "30402" --nodiscover \
-  --ipcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
   --ipcpath "$HOME/myetherdata/testdata2/geth.ipc" \
   --rpcapi "admin,db,eth,debug,miner,net,ssh,txpool,personal,web3" \
-  --autodag --networkid 1900 --nat "any" $*
+  --rpcaddr "0.0.0.0" \
+  --networkid 1900 --nat "any" $*
 
 
