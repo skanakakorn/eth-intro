b2e3fb2de875f4b9d437b250ef27a96a8a77cce5
Nick Kanakakorn <nick@blockfint.com>
2018-08-08 17:19:00 +0700

    fix warnings

--- tweet_storage2.sol
+++ tweet_storage2.sol
@@ -24,18 +24,18 @@ contract TweetStorage {
   );
 
   // constructor
-  function TweetStorage() {
+  constructor() public {
     _tweetCount = 0;
     _adminAddress = msg.sender;
   }
   
   // returns true if caller of function ("sender") is admin
-  function isAdmin() constant returns (bool isAdmin) {
+  function isAdmin() constant public returns (bool yesAdmin) {
     return msg.sender == _adminAddress;
   }
   
   // create new tweet
-  function tweet(string tweetString) returns (int result) {
+  function tweet(string tweetString) public returns (int result) {
     if (bytes(tweetString).length > 256) {
       // limit to 256 bytes
       result = -3;
@@ -46,15 +46,15 @@ contract TweetStorage {
       result = 0; // success
     }
     // Create Tweet event
-    Tweet(msg.sender, tweetString, _tweetCount);
+    emit Tweet(msg.sender, tweetString, _tweetCount);
   }
   
-  function getTweet(uint tweetId) constant returns (
+  function getTweet(uint tweetId) constant public returns (
       string tweetString, address from) {
     return(_tweets[tweetId].tweetString, _tweets[tweetId].from);
   }
   
-  function getLatestTweet() constant returns (string tweetString, uint numberOfTweets, 
+  function getLatestTweet() constant public returns (string tweetString, uint numberOfTweets, 
                                               address from) {
     tweetString = _tweets[_tweetCount - 1].tweetString;
     from = _tweets[_tweetCount - 1].from;
@@ -62,11 +62,11 @@ contract TweetStorage {
     return(tweetString, numberOfTweets, from);
   }
   
-  function getOwnerAddress() constant returns (address adminAddress) {
+  function getOwnerAddress() constant public returns (address adminAddress) {
     return _adminAddress;
   }
   
-  function getNumberOfTweets() constant returns (uint numberOfTweets) {
+  function getNumberOfTweets() constant public returns (uint numberOfTweets) {
     return _tweetCount;
   }
 }
